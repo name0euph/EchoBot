e1ca55138c5ce18964446fafe0f105bd796db564
name0euph <name0euph@gmail.com>
2024-02-15 23:40:22 +0900

    SK APIへのPOSTリクエストを実装

--- Bots/EchoBot.cs
+++ Bots/EchoBot.cs
@@ -1,20 +1,53 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-using System.Collections.Generic;
-using System.Threading;
-using System.Threading.Tasks;
 using Microsoft.Bot.Builder;
 using Microsoft.Bot.Schema;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Microsoft.BotBuilderSamples.Bots
 {
     public class EchoBot : ActivityHandler
     {
+        // HTTPクライアントを作成して初期化
+        private static readonly HttpClient client = new HttpClient();
+
+
         protected override async Task OnMessageActivityAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
         {
-            var replyText = $"Echo: {turnContext.Activity.Text}";
-            await turnContext.SendActivityAsync(MessageFactory.Text(replyText, replyText), cancellationToken);
+            try
+            {
+                Console.WriteLine(turnContext.Activity.Text);
+
+                //リクエストボディを作成
+                var json = $"{{ \"text\" : \"{turnContext.Activity.Text}\" }}";
+                var content = new StringContent(json, Encoding.UTF8, @"application/json");
+
+                //POSTリクエストを送信
+                var response = await client.PostAsync(
+                    "https://semantickernelapi20240215022752.azurewebsites.net/api/HttpExample",
+                    content,
+                    cancellationToken
+                    );
+
+                //エラーチェック
+                response.EnsureSuccessStatusCode();
+
+                //レスポンスを文字列として取得
+                var replyText = await response.Content.ReadAsStringAsync();
+
+                await turnContext.SendActivityAsync(MessageFactory.Text(replyText, replyText), cancellationToken);
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
         }
 
         protected override async Task OnMembersAddedAsync(IList<ChannelAccount> membersAdded, ITurnContext<IConversationUpdateActivity> turnContext, CancellationToken cancellationToken)
